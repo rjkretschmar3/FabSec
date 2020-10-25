5abd5ff936364c166c595b93b8af57016e4b848f
Robert Kretschmar <rkretschmar3@gmail.com>
2020-10-25 00:36:14 -0700

    Biggest thing this commit is created the 004 script to deploy the Fabric CA Server. Otherwise, fixed the scripts to have the lines capped at 90 columns or less for readability.

--- scripts/001-deploy-new-tls-ca-server.sh
+++ scripts/001-deploy-new-tls-ca-server.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+# Written and maintained by Robert Kretschmar
 
-# This script will initialize and deploy an Organization's TLS CA Server. It assumes a few things:
+# This script will initialize and deploy an Organization's TLS CA Server. It assumes a few
+# things:
 #	Commandline Arguments are positional and are as follows:
 #		1) Whether it is an ~orderer~ or ~peer~ organization.
-#		2) The ID number of the organization. (In the real world, this would be an actual name.)
+#		2) The ID number of the organization. (In the real world, this would be 
+#		an actual name.)
 #
-#	The directory infrastructure is already in place. For example, a peer organization of ID 1 would have the directory structure of organizations/peerOrganizations/org1.fabsec.com 
+# 	The directory infrastructure is already in place. For example, a peer organization
+# of ID 1 would have the directory structure of 
+# organizations/peerOrganizations/org1.fabsec.com, and specifically the directory 
+# tls-ca-server under that tree. 
 #
-#	Furthermore, that the fabric-ca-server binary is located in the tls-ca-server directory under org1.fabsec.com (or whichever organization you're working with).
+#	Furthermore, that the fabric-ca-server binary is located in the tls-ca-server 
+# directory under org1.fabsec.com (or whichever organization you're working with).
 
 # ADDED TO NOT MAKE THIS MISTAKE AGAIN:
 read -p "This script is for initializing a new TLS server. If you have an existing TLS server, this will destroy the old key material , not to mention the YAML file, requiring all participates to re-register and re-enroll. If you have an initialized server already, use the non-destructive start-tls-server.sh script instead. Are you sure you want to continue? [y/N] " prompt
@@ -17,7 +24,8 @@ if [[ $prompt != "y" && $prompt != "Y" ]]; then
 	exit;
 fi
 
-# First move to the appropriate directory: organizations/ordererOrganizations for an Orderer and organizations/peerOrganizations for a Peer.
+# First move to the appropriate directory: organizations/ordererOrganizations for an 
+# Orderer and organizations/peerOrganizations for a Peer.
 if [[ "$1" == "orderer" ]]; then
 	echo "cd ../organizations/ordererOrganizations/org$2.fabsec.com/tls-ca-server";
 	cd ../organizations/ordererOrganizations/org$2.fabsec.com/tls-ca-server
@@ -32,11 +40,16 @@ else
 fi
 
 # Next, this will initialize the server. Initialization does a few things: 
-#	1) It will register a bootstrap identity, which will be in the form of <ADMIN_USER>:<ADMIN_PW>. In the case of the above example the bootstrap identity will be org1-tls-admin:org1-tls-adminpw.
-#	2) It'll set the default CA Home directory environmental variable FABRIC_CA_HOME to the present working directory. (Thus, the need to traverse there in the previous steps.)
+#	1) It will register a bootstrap identity, which will be in the form of 
+# <ADMIN_USER>:<ADMIN_PW>. In the case of the above example the bootstrap identity will be
+# org1-tls-admin:org1-tls-adminpw.
+#	2) It'll set the default CA Home directory environmental variable FABRIC_CA_HOME 
+# to the present working directory. (Thus, the need to traverse there in the previous 
+# steps.)
 #	3) Generates the default YAML configuration file. (Talked about later.)
 #	4) Generates the TLS CA root signed certificate file ca-cert.pem
-#	5) Generates the TLS CA server private key and stores it in the FABRIC_CA_HOME directory under /msp/keystore
+#	5) Generates the TLS CA server private key and stores it in the FABRIC_CA_HOME 
+# directory under /msp/keystore
 
 # Remove the old config file and the old key material
 echo "rm ./fabric-ca-server-config.yaml"
@@ -50,7 +63,11 @@ echo "./fabric-ca-server init -b org$2-tls-admin:org$2-tls-admin-pw";
 echo "org$2-tls-admin:org$2-tls-admin-pw" > tls-creds.txt
 ./fabric-ca-server init -b org$2-tls-admin:org$2-tls-admin-pw
 
-# Here is where it gets murky. As mentioned this initialization process generates a default YAML file for server configuration. I have yet to find an elegant way to automatically change some of the default values to the working ones. For now, it opens up vim for manual editing. (However, I can generate and present the values for the operator to write down before changing them.)
+# Here is where it gets murky. As mentioned this initialization process generates a default
+# YAML file for server configuration. I have yet to find an elegant way to automatically 
+# change some of the default values to the working ones. For now, it opens up vim for 
+# manual editing. (However, I can generate and present the values for the operator to write
+# down before changing them.) TODO: Research the program 'yq' for this task.
 
 echo "This script will now open the YAML configuration file.";
 echo "Some fields to be changed: ";
@@ -67,7 +84,10 @@ echo "Done!";
 
 # The following section will start the server.
 
-# In case the CSR block of the YAML file has been changed in the vim step above, delete the TLS CA cert ca-cert.pem and the entire tls-ca-server/msp directory. These will be regenerated with the correct information when the servers starts up.
+# In case the CSR block of the YAML file has been changed in the vim step above, delete the
+# TLS CA cert ca-cert.pem and the entire tls-ca-server/msp directory. These will be 
+# regenerated with the newly populated config information when the servers starts up next
+# time.
 
 echo "rm ca-cert.pem";
 rm ca-cert.pem
---
+++ scripts/004-deploy-new-fab-ca-server.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# This script will initialize and deploy an Organization's Fabric CA Server. There is a 
+# bit of linguistic confusion here as the official documentation calls this the Organization 
+# CA Server, but I fear this overloads the word "Organization"... not that Fabric CA Server 
+# is any better, but it is a bit less overloaded.
+#
+# That being said, the purpose of this Fab CA Server, as opposed to the TLS CA Server, is to 
+# manage identities that will be used heavily on the whole of the network. It fills the 
+# "permissioned" quality of the network. I'll explain more as we hit different responsibilities
+# of this server. For now, let's initialize and deploy it.
+#
+# As with the other scripts, we have the commandline arguments being ~orderer~ or ~peer~ and 
+# the ID number of the org in question.
+#
+# Also, like the TLS Deployment script (001), this script assumes the directory structure for
+# the Fab Server of organizations/{orderer|peer}Organizations/org{ID#}.fabsec.com, and 
+# specifically the fab-ca-server directory under that tree.
+
+# First remind the operator that this is a destructive script...
+read -p "This script is for initializing a new Fab CA server. If you have an existing Fab CA server, this will destroy the old key material , not to mention the YAML file, requiring all participates to re-register and re-enroll. If you have an initialized server already, use the non-destructive start-fab-server.sh script instead. Are you sure you want to continue? [y/N] " prompt
+
+if [[ $prompt != "y" && $prompt != "Y" ]]; then
+	echo "A wise choice, exiting...";
+	exit;
+fi
+
+# Next, as always, let's move to the correct working directory.
+if [[ "$1" == "orderer" ]]; then
+        echo "cd ../organizations/ordererOrganizations/org$2.fabsec.com/fab-ca-server";
+        cd ../organizations/ordererOrganizations/org$2.fabsec.com/fab-ca-server;
+elif [[ "$1" == "peer" ]]; then
+        echo "cd ../organizations/peerOrganizations/org$2.fabsec.com/fab-ca-server";
+        cd ../organizations/peerOrganizations/org$2.fabsec.com/fab-ca-server;
+else
+        echo "Unsure if Orderer or Peer organization!";
+        echo "Pro-tip: proper use of this script comes in the form of: ";
+        echo -e "\t$0 <orderer|peer> <ID#>";
+        exit;
+fi
+
+# First grab the credentials from the fab-creds.txt file. 
+echo "IFS=':' read -ra creds <<< \$(cat fab-creds.txt)";
+IFS=':' read -ra creds <<< $(cat fab-creds.txt);
+
+# Next, we want to copy over the organization CA TLS certificate and key pair that was 
+# generated in script 003. The cert.pem is the admin's X.509 certificate which is how 
+# they identifies themselves to the TLS server (in lieu of credentials) and lives in 
+# ca-client/tls-ca/<fab-admin>/msp/signedcerts/ directory. The key.pem, or what we ended up 
+# renaming to key.pem, is the signing key for the admin and lives in a similar location to 
+# above but msp/keystore/ directory. These will be conveniently place in a directory called tls/
+
+echo "cp ../ca-client/tls-ca/${creds[0]}/msp/signcerts/cert.pem tls";
+cp ../ca-client/tls-ca/${creds[0]}/msp/signcerts/cert.pem tls
+echo "cp ../ca-client/tls-ca/${creds[0]}/msp/keystore/key.pem tls";
+cp ../ca-client/tls-ca/${creds[0]}/msp/keystore/key.pem tls
+
+# Now, just like with the 001 script and the TLS CA Server, we're going to initialize the Fab 
+# CA Server and bootstrap its admin.
+
+echo "./fabric-ca-server init -b ${creds[0]}:${creds[1]}"
+./fabric-ca-server init -b ${creds[0]}:${creds[1]}
+
+# That was easy. But, this next part isn't. At least until I research that 'yq' program to 
+# help automate this. The previous step generated the default fabric-ca-server-config.yaml
+# file that we'll need to edit in vim. Again, I'll echo out some helpful details to the 
+# operator before they enter vim to guide them on what to change. (It's a bit more than just
+# what the TLS server had to.)
+
+echo "This script will now open the YAML configuration file.";
+echo "Some fields to be changed: "
+echo -e "\tport - The port to operate on. Needs to be changed to 7055 for the Fab CA Server."
+echo -e "\ttls.enabled - Change to true."
+echo -e "\ttls.certfile - Relative path and filename for the TLS CA signedcert. This is the cert.pem we just copied into the local tls directory."
+echo -e "\ttls.keystore - Relative path and filename fort the TLS CA private key. This is the key.pem we just copied into the local tls directory."
+echo -e "\tca.name - Change to org$2-fab-ca."
+echo -e "\tcsr.hosts - Check that the hostnames are in order."
+echo -e "\toperations.listenAddress - Since we have the TLS CA on this same host we need to change this to 9444."
+echo -e "\tsigning.profiles - Delete the ~tls~ profile."
+read -p "Press [Enter] to enter vim..."
+
+echo "vim fabric-ca-server-config.yaml"
+vim fabric-ca-server-config.yaml
+echo "Done!"
+
+
+#The following section will start the server.
+
+# In case the CSR block of the YAML file has been changed in the vim step above, delete the
+# TLS CA cert ca-cert.pem and the entire tls-ca-server/msp directory. These will be
+# regenerated with the newly populated config information when the servers starts up next
+# time.
+
+echo "rm ca-cert.pem";
+rm ca-cert.pem
+echo "rm -R msp";
+rm -R msp
+
+# Finally start the TLS Server
+echo "./fabric-ca-server start"
+./fabric-ca-server start

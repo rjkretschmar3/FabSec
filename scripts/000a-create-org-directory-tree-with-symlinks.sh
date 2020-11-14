2dee548da9e1fdf0708c8800ed01c03c4aec952b
Robert Kretschmar <rkretschmar3@gmail.com>
2020-11-13 17:09:56 -0800

    In this commit, I have removed the organizations/ directory and, subsequently, all of its children directories. Instead, I've created a new script (000a) to generate this directory tree from scratch. Also, I've added a bin/ directory with all of the binaries (CA Servers, Peers, Orderers, etc) which the aforementioned script will symlink to where needed. Since these binaries do not change from Organization to Organization (it is the individual configuration files that change their behavior), this will keep the project's memory footprint lower. I have also added the config.yaml for Organization 1's NodeOUs functionality and the Org 1's Peer 0 core.yaml file to the test-configs/ directories. Also, a default version of the core.yaml that isn't attached to any peer.

---
+++ scripts/000a-create-org-directory-tree-with-symlinks.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#Written and maintained by Robert Kretschmar
+
+# This is one of the structural scripts as denoted my having the prefix of 000 and a letter. This
+# script in particular sets up the organizational directory structure. It will set up both the Orderer
+# and Peers organizations. It will also make the appropriate symlinks to the CA Server, CA Client, Peer
+# and Orderer binaries.
+
+# As of now, the numbering is static reflecting the topography of one Orderer Org with one Orderer, and
+# two Peers Orgs, one with one Peer and the other with two Peers. An idea for later would be to make the
+# script more dynamic by letting the script operator choose how many of each gets set up with commandline
+# arguments.
+
+# Finally, this script assumes that it will be ran from the fabsec/scripts/ directory and will start the 
+# directory tree at fabsec/organizations/ (Further assumes organizations/ directory tree doesn't already 
+# exist.)
+
+echo "This script will set up the organizational directory tree as well as symlink the appropriate " \
+       "binaries from the fabsec/bin/ directory.";
+
+# Start with the top of the tree.
+echo "Moving to root directory, and creating organizations/ directory.";
+cd ..
+mkdir organizations/
+
+# Set up Orderer Organization.
+echo "Making Orderer Organization: ordererOrganizations/org0.fabsec.com/"
+cd organizations/
+mkdir ordererOrganizations/
+cd ordererOrganizations/
+mkdir org0.fabsec.com/
+cd org0.fabsec.com/
+
+# Set up orderers/
+echo "Setting up orderers/ directory with orderer0.org0.fabsec.com..."
+mkdir orderers/
+cd orderers/
+mkdir orderer0.org0.fabsec.com/
+cd orderer0.org0.fabsec.com/
+ln -s ../../../../../bin/orderer
+cd ../../
+
+# Set up ca-client/
+echo "Setting up ca-client/ directory..."
+mkdir ca-client/
+cd ca-client/
+mkdir fab-ca/
+mkdir tls-ca/
+mkdir tls-root-cert/
+ln -s ../../../../bin/fabric-ca-client
+cd ../
+
+# Set up fab-ca-server/
+echo "Setting up fab-ca-server/ directory..."
+mkdir fab-ca-server/
+cd fab-ca-server/
+mkdir tls/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# Set up tls-ca-server/
+echo "Setting up tls-ca-server/ directory..."
+mkdir tls-ca-server/
+cd tls-ca-server/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# Return to organizations
+cd ../../
+
+# Set up first Peer Organization.
+echo "Making Peer Organization: peerOrganizations/org1.fabsec.com/"
+mkdir peerOrganizations/
+cd peerOrganizations/
+mkdir org1.fabsec.com/
+cd org1.fabsec.com/
+
+# Set up peers/
+echo "Setting up peers/ directory with peer0.org1.fabsec.com..."
+mkdir peers/
+cd peers/
+mkdir peer0.org1.fabsec.com/
+cd peer0.org1.fabsec.com/
+ln -s ../../../../../bin/peer
+cd ../../
+
+# Set up ca-client/
+echo "Setting up ca-client/ directory..."
+mkdir ca-client/
+cd ca-client/
+mkdir fab-ca/
+mkdir tls-ca/
+mkdir tls-root-cert/
+ln -s ../../../../bin/fabric-ca-client
+cd ../
+
+# Set up fab-ca-server/
+echo "Setting up fab-ca-server/ directory..."
+mkdir fab-ca-server/
+cd fab-ca-server/
+mkdir tls/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# Set up tls-ca-server/
+echo "Setting up tls-ca-server/ directory..."
+mkdir tls-ca-server/
+cd tls-ca-server/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# Return to peerOrganizations/
+cd ../
+
+# Set up second Peer Organization.
+echo "Making Peer Organization: peerOrganizations/org2.fabsec.com/"
+mkdir org2.fabsec.com/
+cd org2.fabsec.com/
+
+# Set up peers/
+echo "Setting up peers/ directory with peer0.org2.fabsec.com/ and peer1.org2.fabsec.com/..."
+mkdir peers/
+cd peers/
+mkdir peer0.org2.fabsec.com/
+cd peer0.org2.fabsec.com/
+ln -s ../../../../../bin/peer
+cd ../
+mkdir peer1.org2.fabsec.com/
+cd peer1.org2.fabsec.com/
+ln -s ../../../../../bin/peer
+cd ../../
+
+# Set up ca-client/
+echo "Setting up ca-client/ directory..."
+mkdir ca-client/
+cd ca-client/
+mkdir fab-ca/
+mkdir tls-ca/
+mkdir tls-root-cert/
+ln -s ../../../../bin/fabric-ca-client
+cd ../
+
+# Set up fab-ca-server/
+echo "Setting up fab-ca-server/ directory..."
+mkdir fab-ca-server/
+cd fab-ca-server/
+mkdir tls/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# Set up tls-ca-server/
+echo "Setting up tls-ca-server/ directory..."
+mkdir tls-ca-server/
+cd tls-ca-server/
+ln -s ../../../../bin/fabric-ca-server
+cd ../
+
+# ...and we're done!
+echo "Finished!"
+
+
